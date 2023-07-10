ea5ac75a008747a0f656c6c6ea63e9a481811bd3
Jonas Busto <jonasrex00@gmail.com>
2023-07-10 20:15:56 -0300

    correcciones de bugs

--- script/login.js
+++ script/login.js
@@ -9,6 +9,7 @@ import {
   FacebookAuthProvider,
   updateProfile,
   sendPasswordResetEmail,
+  OAuthProvider,
 } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
 import {
   setDoc,
@@ -16,6 +17,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js";
 
 const btnLoginGoogle = document.getElementById("btn-login-google");
+// const btnLoginMicrosoft = document.getElementById("btn-login-microsoft");
 
 const loginWithGoogle = async () => {
   const googleProvider = new GoogleAuthProvider();
@@ -30,4 +32,18 @@ const loginWithGoogle = async () => {
     .catch((error) => console.log("google error: ", error));
 };
 
+// const loginWithMicrosoft = async () => {
+//   const microsoftProvider = new OAuthProvider("microsoft.com");
+//   signInWithPopup(auth, microsoftProvider)
+//     .then(async (currentUser) => {
+//       await setDoc(doc(db, "users", currentUser.user.uid), {
+//         email: currentUser.user.email,
+//         rol: "user",
+//       });
+//       window.location.href = "/";
+//     })
+//     .catch((error) => console.log("google error: ", error));
+// };
+
 btnLoginGoogle.addEventListener("click", loginWithGoogle);
+// btnLoginMicrosoft.addEventListener("click", loginWithMicrosoft);
