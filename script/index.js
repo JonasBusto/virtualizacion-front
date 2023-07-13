016d973164deb36747013e7e6a356ddbd6662d62
Jonas Busto <jonasrex00@gmail.com>
2023-07-13 01:10:03 -0300

    algunos cambios

--- script/index.js
+++ script/index.js
@@ -55,40 +55,160 @@ const cargarInfoPersonal = () => {
 
   divInfoPersonal.innerHTML = `
   <div class="d-flex flex-column info">
-        <div>
+        <div> 
+        <div class="modal-container">
           <button data-bs-toggle="modal" data-bs-target="#editarPresentacion">
             Editar
           </button>
-          <div class="modal fade" id="editarPresentacion" tabindex="-1" aria-labelledby="editarPresentacionLabel" aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              
-              <div class="modal-body">
-                <input type="text" name="" id="input-presentacion-editar" value=${
-                  estudiante.presentacion
-                }>
-                <div>
-                  <button data-bs-dismiss="modal">Cancelar</button>
-                  <button id="btn-editar-presentacion">Guardar</button>
-                </div>
-              </div>  
-            </div>  
-          </div>
-        </div>
 
-        <div> 
+          <div
+            class="modal fade"
+            id="editarPresentacion"
+            tabindex="-1"
+            aria-labelledby="editarPresentacionLabel"
+            aria-hidden="true"
+          >
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-body">
+                  <div class="modal-div-input">
+                    <p>Modificar presentación</p>
+                    <div class="container-form-presentacion d-flex flex-column">
+                    <p>Email:</p>
+                    <input type="text" placeholder="Nueva información" value=${
+                      estudiante.emails.institucional
+                    }/>
+                  </div>
+                  <div class="container-form-presentacion d-flex flex-column">
+                    <p>Presentación:</p>
+                    <textarea name="" id="" cols="30" rows="10">${
+                      estudiante.presentacion
+                    }</textarea>
+                  </div>
+                  <div class="container-form-presentacion d-flex flex-column">
+                    <p>Url actual Imagen:</p>
+                    <img
+                      class="img-fluid"
+                      src="${estudiante.foto_url}"
+                      alt=""
+                    />
+                    <input type="text" value="${
+                      estudiante.foto_url
+                    }" placeholder="URL Imagen" />
+                  </div>
+                  </div>
+                  <div class="d-flex justify-content-between btn-container-modal">
+                    <button data-bs-dismiss="modal">Cancelar</button>
+                    <button id="btn-editar-presentacion">Guardar</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
           <p class="mt-2">
             ${estudiante.presentacion}
           </p>
         </div>
+        
         <div class="d-flex mt-4">
           <div
             class="d-flex flex-column align-items-start p-title acerca w-100"
           >
-            <p>Acerca de mi</p>
+          <div class="acerca d-flex align-items-center">
+            <p>Sobre mi</p>
+            <div class="modal-container d-flex align-items-center">
+          <button class="btn-e-sobre-mi" data-bs-toggle="modal" data-bs-target="#agregarSobreMi">
+            (Editar)
+          </button>
+
+          <div
+            class="modal fade"
+            id="agregarSobreMi"
+            tabindex="-1"
+            aria-labelledby="agregarSobreMiLabel"
+            aria-hidden="true"
+          >
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-body">
+                  <div class="modal-div-input">
+                    <p>Agregar algo sobre mi</p>
+                    <textarea name="" id="textarea-sobre-mi-agregar" cols="30" rows="10"></textarea>
+                  </div>
+                  <div class="d-flex justify-content-between btn-container-modal">
+                    <button data-bs-dismiss="modal">Cancelar</button>
+                    <button id="btn-agregar-sobre-mi">Guardar</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+          </div>
             <div class="container-list">
               <ul>
-                <li>Mi nombre es ${estudiante.nombre_completo}</li>
+                <li class="d-flex align-items-center">Mi nombre es ${
+                  estudiante.nombre_completo
+                }
+                <div class="modal-container">
+        <button data-bs-toggle="modal" data-bs-target="#borrarInfoSobreMi">
+          (Borrar)
+        </button>
+
+        <div
+          class="modal fade"
+          id="borrarInfoSobreMi"
+          tabindex="-1"
+          aria-labelledby="borrarInfoSobreMiLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-body">
+                <div class="modal-div-input">
+                  <p>Borrar esta información:</p>
+                  <p class="p-borrar-sobre-mi">'Item a borrar'</p>
+                </div>
+                <div class="d-flex justify-content-between btn-container-modal">
+                  <button data-bs-dismiss="modal">Cancelar</button>
+                  <button>Borrar</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal-container">
+        <button data-bs-toggle="modal" data-bs-target="#modificarInfoSobreMi">
+          (Editar)
+        </button>
+
+        <div
+          class="modal fade"
+          id="modificarInfoSobreMi"
+          tabindex="-1"
+          aria-labelledby="modificarInfoSobreMiLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-body">
+                <div class="modal-div-input">
+                  <p>Modificar esta información:</p>
+                  <textarea name="" id="textarea-sobre-mi-agregar" cols="30" rows="10"></textarea>
+                </div>
+                <div class="d-flex justify-content-between btn-container-modal">
+                  <button data-bs-dismiss="modal">Cancelar</button>
+                  <button>Guardar</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+                
+                </li>
                 <li>Tengo ${estudiante.edad} años</li>
                 <li>
                   Conocimientos en:
@@ -106,25 +226,45 @@ const cargarInfoPersonal = () => {
           </div>
         </div>
         <div class="contacto-email">
+          
           <p>Me puedes contactar por:</p>
+          
           <p class="d-flex justify-content-end">
             📨
             <a href=${"mailto:" + estudiante.emails.institucional}
               >${estudiante.emails.institucional}</a
             >
           </p>
+          
         </div>
   `;
 
-  const presentacionEditada = document.getElementById(
-    "input-presentacion-editar"
+  const inputSobreMiAgregar = document.getElementById(
+    "textarea-sobre-mi-agregar"
   );
 
-  document
-    .getElementById("btn-editar-presentacion")
-    .addEventListener("click", () => {
-      console.log(presentacionEditada.value);
-    });
+  const btnGuardarPresentacion = document.getElementById(
+    "btn-editar-presentacion"
+  );
+
+  const btnAgregarSobreMi = document.getElementById("btn-agregar-sobre-mi");
+  btnAgregarSobreMi.classList.add("btn-disabled");
+
+  btnGuardarPresentacion.addEventListener("click", () => {
+    console.log(presentacionEditada.value);
+  });
+
+  btnAgregarSobreMi.addEventListener("click", () => {
+    console.log(inputSobreMiAgregar.value);
+  });
+
+  inputSobreMiAgregar.addEventListener("keyup", () => {
+    if (inputSobreMiAgregar.value.trim() === "") {
+      btnAgregarSobreMi.classList.add("btn-disabled");
+    } else {
+      btnAgregarSobreMi.classList.remove("btn-disabled");
+    }
+  });
 };
 
 const cargarHerramientas = () => {
